96998fc4d03e5a2f86b3a4b551b2ac900791d20f
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2021-11-09 18:04:06 +0100

    ✨ Add status-based filter for event fetcher

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -27,15 +27,36 @@ class EventController extends Controller
      */
     public function index()
     {
+
         $page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
         $range = filter_input(INPUT_GET, "range", FILTER_SANITIZE_NUMBER_INT);
 
         $start_id = $range * $page;
         $end_id = $start_id + $range + 1;
 
-        $styles = (is_null($page) || is_null($range)) ? Event::all() : Event::where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+        $status = filter_input(INPUT_GET, "status", FILTER_SANITIZE_STRING);
 
-        return response([ 'events' => EventResource::collection($styles), 'message' => 'Retrieved successfully'], 200);
+        if ($status === 'incoming') {
+            $events = (is_null($page) || is_null($range)) ?
+                Event::where('end_date', '>', Carbon::now())->get() :
+                Event::where('end_date', '>', Carbon::now())->get()->where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+
+            return response([ 'events' => EventResource::collection($events), 'message' => 'Retrieved successfully'], 200);
+        }
+
+        if ($status === 'archived') {
+            $events = (is_null($page) || is_null($range)) ?
+                Event::where('end_date', '<', Carbon::now())->get() :
+                Event::where('end_date', '<', Carbon::now())->get()->where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+
+            return response([ 'events' => EventResource::collection($events), 'message' => 'Retrieved successfully'], 200);
+        }
+
+        $events = (is_null($page) || is_null($range)) ?
+            Event::all() :
+            Event::where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+
+        return response([ 'events' => EventResource::collection($events), 'message' => 'Retrieved successfully'], 200);
     }
 
     /**
