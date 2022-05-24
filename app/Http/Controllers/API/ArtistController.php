5ea5fe09f73cf510ed04dba70bebe12a39de4afe
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2022-05-24 19:55:35 +0200

    🚸 Add 500 Internal Server Errors for GET /artists

--- app/Http/Controllers/API/ArtistController.php
+++ app/Http/Controllers/API/ArtistController.php
@@ -16,7 +16,21 @@ class ArtistController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function count() {
-        return Artist::all()->count();
+        try {
+            $page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+            $range = filter_input(INPUT_GET, "range", FILTER_SANITIZE_NUMBER_INT);
+
+            $start_id = $range * $page;
+            $end_id = $start_id + $range + 1;
+
+            $artists = (is_null($page) || is_null($range))
+                ? Artist::all()
+                : Artist::where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+
+            return response([ 'artists' => ArtistResource::collection($artists)], 200);
+        } catch (\Exception $e) {
+            return response(['error' => $e ? $e : 'An error has occurred'], 500);
+        }
     }
 
     /**
