c4fe5780367174168fe0a0c2d1d2da9a32e80a2f
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2022-05-24 19:55:47 +0200

    🚸 Add 500 Internal Server Error for GET /events/

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -17,7 +17,21 @@ class EventController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function count() {
-        return Event::all()->count();
+        try {
+            $page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+            $range = filter_input(INPUT_GET, "range", FILTER_SANITIZE_NUMBER_INT);
+
+            $start_id = $range * $page;
+            $end_id = $start_id + $range + 1;
+
+            $events = (is_null($page) || is_null($range))
+                ? Event::all()
+                : Event::where('id', '>', $start_id)->where('id', '<', $end_id)->get();
+
+            return response([ 'events' => EventResource::collection($events)], 200);
+        } catch (\Exception $e) {
+            return response(['error' => $e ? $e : 'An error has occurred'], 500);
+        }
     }
 
     /**
